08b7adf8bb0b32466f4c640b727ef5dd080ff1ff
fabburst <40134787+fabburst@users.noreply.github.com>
2024-08-02 07:58:22 +0200

    fix: Handle trailing and leading semicolon, colon, and their combinations in word comparisons
    - Added functionality to ignore trailing and leading semicolon (;) and colon (:) in word comparisons.
    - Updated normalizeText function to remove specified punctuations and handle combinations of these characters.
    - Ensured consistent comparison of user input with the original words by trimming and normalizing text.
    - Improved the user experience by accurately validating words regardless of trailing or leading semicolon, colon, and their combinations with spaces and other punctuations.
    
    This commit addresses the issue of punctuation affecting word validation.

--- script.js
+++ script.js
@@ -132,8 +132,8 @@ document.addEventListener('DOMContentLoaded', function () {
         return text
             .normalize('NFD')
             .replace(/[\u0300-\u036f]/g, '') // Remove accents
-            .replace(/['.,?():;]/g, '') // Remove specific punctuations
-            .replace(/^[.,?():;]+|[.,?():;]+$/g, '') // Remove punctuations at the beginning and end of the word
+            .replace(/[.,?():;]+/g, '') // Remove specific punctuations
+            .replace(/^\s*[.,?():;]+\s*|\s*[.,?():;]+\s*$/g, '') // Remove punctuations at the beginning and end of the word, with or without spaces
             .toLowerCase()
             .replace(/'/g, ''); // Replace apostrophes
     }
